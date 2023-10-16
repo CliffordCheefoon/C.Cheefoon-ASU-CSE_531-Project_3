a924b63b7a4d447c3ae38bc1542c4e63164039be
Clifford Cheefoon <cheefoonclifford@yahoo.com>
2023-10-15 21:34:54 -0400

    another tidy pass

--- main.py
+++ main.py
@@ -14,7 +14,7 @@ BRANCH_SERVER_LOG_DIR :str = """tests/server_out/"""
 def main(input_file_dir : str):
     input_json = json.load( open(input_file_dir, 'r', encoding="UTF8"))
     branches_inputs = get_branches(input_json, logging.getLogger())
-    #customer_inputs = get_customers(input_json, logging.getLogger())
+    customer_inputs = get_customers(input_json, logging.getLogger())
     branch_server_spawn_manager_instance = branch_server_spawn_manager()
     branch_server_spawn_manager_instance.assign_ports(branches_inputs, PORT_OFFSET)
     for branches_input in branches_inputs:
